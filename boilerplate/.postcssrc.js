110ba8f662819948bc14624f2cdd69cb5ff1c907
jimblue <mr.jimblue@gmail.com>
2018-12-05 17:34:27 +0100

    update boilerplate (move from cssnano to csso)

--- boilerplate/.postcssrc.js
+++ boilerplate/.postcssrc.js
@@ -1,7 +1,7 @@
 const mqpacker = require('css-mqpacker')
 const sortCSSmq = require('sort-css-media-queries')
 const postcssPresetEnv = require('postcss-preset-env')
-const cssnano = require('cssnano')
+const csso = require('postcss-csso');
 const purgecss = require('@fullhuman/postcss-purgecss')
 
 module.exports = ({ env }) => ({
@@ -9,16 +9,6 @@ module.exports = ({ env }) => ({
     mqpacker({
       sort: sortCSSmq
     }),
-    env === 'production' ? cssnano({
-      preset: [
-        'default',
-        {
-          discardComments: {
-            removeAll: true
-          }
-        }
-      ]
-    }) : false,
     env === 'production' ? purgecss({
       content: [
         './layouts/*.html',
@@ -26,6 +16,9 @@ module.exports = ({ env }) => ({
         './partials/**/*.html'
       ],
       keyframes: true
+    }) : false,
+    env === 'production' ? csso({
+      comments: false
     }) : false,
     postcssPresetEnv({
       features: {
